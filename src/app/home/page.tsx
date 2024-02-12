d178a6c42d25a2c71516f5f12a3eb2ec6e389e18
TetrisBattle <thien5596@gmail.com>
2024-02-13 01:00:47 +0200

    Remove eslint-plugin-thng

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,14 +1,12 @@
 'use client'
 
-import { Box, Button, Typography } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import { observer } from 'mobx-react-lite'
 
 function Home() {
 	return (
 		<Box sx={{ p: 2 }}>
 			<Typography variant='h1'>Home</Typography>
-			<Button>Button</Button>
-			<Button variant='contained'>Button</Button>
 		</Box>
 	)
 }
