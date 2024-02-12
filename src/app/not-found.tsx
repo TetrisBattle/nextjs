138aac9a857e366e4e50098a3bbf8b6e0ad3c5b0
TetrisBattle <thien5596@gmail.com>
2024-02-12 21:20:31 +0200

    Add notFound page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import { Box, Typography } from '@mui/material'
+
+export default function NotFound() {
+	return (
+		<Box sx={{ p: 5, textAlign: 'center' }}>
+			<Typography variant='h1'>Error 404</Typography>
+		</Box>
+	)
+}
